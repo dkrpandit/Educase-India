9fbd4f32811765fc10e102f19d9dd72d211395a8
Dharmendra Pandit <dharmendra763297@gmail.com>
2024-08-22 23:49:45 +0530

    changes in port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./src/app");
 
 const startServer = async () => {
-    const port = 3000;
+    const port = config.port || 3000;
     app.listen(port, () => {
         console.log(`Server running on port ${port}`);
     });
